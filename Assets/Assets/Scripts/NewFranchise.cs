0d7b52b7ad85788a5695a5ce9ed749955fefdf8f
Conner Christopherson <cdcqq7@gmail.com>
2018-11-14 22:18:02 -0600

    First commit, some basic stuff is already done with UI

---
+++ Assets/Assets/Scripts/NewFranchise.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewFranchise : MonoBehaviour {
+
+    private string NewFranchiseScene = "NewFranchise";
+
+	public void NewFranchiseClick()
+    {
+        SceneManager.LoadScene(NewFranchiseScene);
+    }
+}
